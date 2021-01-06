c3da296b7ec3c73f19ddef14c8dbf98461ab13cc
StarnyC <43118715+SunnyChaturvedi@users.noreply.github.com>
2021-01-06 17:51:11 +0530

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -1,6 +1,4 @@
-import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
